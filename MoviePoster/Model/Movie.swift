7f7694f830a6c3adf37ec03acc0affceb512c58b
furkan SAKIZCI <fursakizci@gmail.com>
2020-07-23 12:07:21 +0300

    listing completed

--- MoviePoster/Model/Movie.swift
+++ MoviePoster/Model/Movie.swift
@@ -11,23 +11,54 @@ import Foundation
 struct Movie: Codable {
     let title: String
     let year: String
+    let released : String
+    let runTime : String
+    let genre : String
+    let director : String
+    let writer : String
+    let actors : String
+    let plot: String
+    let language : String
+    let country : String
+    let awards : String
+    
+    let imdbID: String
     let poster: String
     let type: String
-    let imdbID: String
-    let plot: String
-
+    
     enum CodingKeys: String, CodingKey {
         case title = "Title"
         case year = "Year"
-        case imdbID
-        case type = "Type"
-        case poster = "Poster"
+        case released = "Released"
+        case runTime = "Runtime"
+        case genre = "Genre"
+        case director = "Director"
+        case writer = "Writer"
+        case actors = "Actors"
         case plot = "Plot"
+        case language = "Language"
+        case country = "Country"
+        case awards = "Awards"
+        
+        case imdbID
+        case poster = "Poster"
+        case type = "Type"
     }
     
     var detail: [String : String] {
         [
-            "Plot" : plot
+            "Title" : title,
+            "Year" : year,
+            "Released" : released,
+            "Runtime" : runTime,
+            "Genre" : genre,
+            "Director" : director,
+            "Writer" : writer,
+            "Actors" : actors,
+            "Plot" : plot,
+            "Language" : language,
+            "Country" : country,
+            "Awards" : awards
         ]
     }
 }
